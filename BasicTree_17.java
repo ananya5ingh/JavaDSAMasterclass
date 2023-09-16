57558d2e41ef8f93087a01578d2fba5d55a5f667
ananya5ingh <ananyasingh2107@gmail.com>
2023-09-16 11:41:49 +0530

    Update BasicTree

---
+++ BasicTree_17.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+class TreeNode{
+
+    String data;
+    ArrayList<TreeNode> children;
+
+    public TreeNode(String data){
+
+        this.data = data;
+        children = new ArrayList<>();
+    }
+
+    public void addChild(TreeNode node){
+        
+    }
+}
+
+
+
+
+
+
+public class BasicTree_17 {
+    
+    public static void main(String[] args){
+
+    }
+}
