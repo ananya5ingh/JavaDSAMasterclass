f233d4b8582153ef2b610c797eb0cfd007417135
ananya5ingh <ananyasingh2107@gmail.com>
2023-10-02 21:06:43 +0530

    progress

---
+++ BinaryTreeArray_19.java
@@ -0,0 +1,105 @@
+class BinaryTree{
+
+    String[] arr;
+    int lastUsedIndex; // needed to track which cell was last used,
+                    // makes life easier when inserting a new node
+    
+    public BinaryTree(int size){
+        arr = new String[size+1]; // since not using 1st cell
+        lastUsedIndex = 0;
+        System.out.println("Blank tree of size " +size+ " has been created.");
+    }
+    // TC: O(1) SC: O(n)
+
+    // isFull method
+    boolean isFull(){
+        if(arr.length-1 == lastUsedIndex){
+            return true;
+        }
+        else{
+            return false;
+        }
+    } // TC: O(1) // SC: O(1)
+
+    void insert(String value){
+        if(!isFull()){
+            arr[lastUsedIndex+1] = value;
+            lastUsedIndex++;
+            System.out.println("The value of " +value+ " has been inserted.");
+        }
+        else{
+            System.out.println("The BT is full.");
+        }
+    } // TC: O(1) // SC: O(1)
+
+    public void preOrder(int index){
+        
+        if(index>lastUsedIndex){
+            return;
+        }
+        System.out.print(arr[index]+ "  ");
+        preOrder(index*2); // TC: O(n/2) // recursion
+        preOrder((index*2)+1); // TC: O(n/2) // recursion
+        
+    } // TC: O(n) // SC: O(n) (stack space)
+
+    public void inOrder(int index){
+        
+        if(index>lastUsedIndex){
+            return;
+        }
+        inOrder(index*2); // TC: O(n/2) // recursion
+        System.out.print(arr[index]+ "  ");
+        inOrder((index*2)+1); // TC: O(n/2) // recursion
+        
+    } // TC: O(n) // SC: O(n)
+
+    public void postOrder(int index){
+        
+        if(index>lastUsedIndex){
+            return;
+        }
+        postOrder(index*2); // TC: O(n/2) // recursion
+        postOrder((index*2)+1); // TC: O(n/2) // recursion
+        System.out.print(arr[index]+ "  ");
+    } // TC: O(n) // SC: O(n)
+
+    public void levelOrder(){
+
+        for(int i=1; i<= lastUsedIndex; i++){
+            System.out.print(arr[i]+ "  ");
+        }
+    } // TC: O(n) // SC: O(1)
+
+}
+
+
+public class BinaryTreeArray_19 {
+    public static void main(String[] args){
+
+        BinaryTree newBinaryTree = new BinaryTree(9);
+
+        newBinaryTree.insert("N1");
+        newBinaryTree.insert("N2");
+        newBinaryTree.insert("N3");
+        newBinaryTree.insert("N4");
+        newBinaryTree.insert("N5");
+        newBinaryTree.insert("N6");
+        newBinaryTree.insert("N7");
+        newBinaryTree.insert("N8");
+        newBinaryTree.insert("N9");
+
+        newBinaryTree.preOrder(1);
+        System.out.println();
+
+        newBinaryTree.inOrder(1);
+        System.out.println();
+
+        newBinaryTree.postOrder(1);
+        System.out.println();
+
+        newBinaryTree.levelOrder();
+        System.out.println();
+        
+    }
+}
--- BinaryTreeLL_18.java
+++ BinaryTreeLL_18.java
@@ -57,6 +57,11 @@ class BinaryTreeLLOperations{
 
     public void levelOrderTraversal(){
 
+        if(root == null){
+            System.out.println("This BT does not exist.");
+            return;
+        }
+
         Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
         // ^creating queue using LL
 
@@ -92,7 +97,7 @@ class BinaryTreeLLOperations{
 
             BinaryNode presentNode = queue.remove(); // creating a node (presentNode) to save the first value of the queue
             if(presentNode.value == valueToFind){
-                System.out.print(presentNode.value + " node found in BT");
+                System.out.print(presentNode.value + " node found in BT\n");
                 return;
             }
             
@@ -152,7 +157,83 @@ class BinaryTreeLLOperations{
             }
         }
     }
-        
+    // TC: O(n) SC: O(n )
+
+    // Deleting a Node - 4 steps
+
+    // Step 1 - Get deepest Node
+    public BinaryNode getDeepestNode(){
+
+        Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
+        queue.add(root);
+        BinaryNode presentNode = null;
+        while(!queue.isEmpty()){
+            presentNode = queue.remove();
+            if(presentNode.left != null){
+                queue.add(presentNode.left);
+            }
+            if(presentNode.right != null){
+                queue.add(presentNode.right);
+            }
+        }
+        return presentNode; 
+    }
+
+    // Step 2 - delete  deepest node
+    public void deleteDeepestNode(){
+        Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
+        queue.add(root);
+        BinaryNode previousNode, currentNode = null;
+        while(!queue.isEmpty()){
+            previousNode = currentNode;
+            currentNode = queue.remove();
+            if(currentNode.left == null){
+                previousNode.right = null;
+                return;
+            }
+            else if(currentNode.right == null){
+                currentNode.left = null;
+                return;
+            }
+            queue.add(currentNode.left);
+            queue.add(currentNode.right);
+        }
+    }
+
+    // Step 3 - Delete given Node
+    void deleteNode(String value){
+        Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            BinaryNode currentNode = queue.remove();
+            if(currentNode.value == value){
+                currentNode.value = getDeepestNode().value;
+                deleteDeepestNode();
+                System.out.println("The node has been deleted.");
+                return;
+            }
+            else{
+                if(currentNode.left != null){
+                    queue.add(currentNode.left);
+                }
+                if(currentNode.right != null){
+                    queue.add(currentNode.right);
+                }
+            }
+        }
+        System.out.println("The node does not exist in this BT");
+    } // TC: O(n) SC: O(n)
+
+    // TC not O(n^2) despite the other methods being in the while loop cause those methods aren't 
+    // being called for each element in the queue mentioned in the while loop (called only for one element)
+
+    // SC O(n) cause we are creating a queue and inserting values into it
+    
+
+     void deleteBT(){
+        root = null;
+        System.out.println("BT successfully deleted");
+     } // TC: O(1) SC: O(1)
 }
 
 public class BinaryTreeLL_18 {
@@ -221,6 +302,28 @@ public class BinaryTreeLL_18 {
         System.out.println("\n");
 
         binaryTree.binaryTreeSearch("N10");
+
+        System.out.println(binaryTree.getDeepestNode().value);
+
+        binaryTree.levelOrderTraversal();
+        System.out.println();
+
+        binaryTree.deleteDeepestNode();
+        binaryTree.levelOrderTraversal();
+        System.out.println();
+
+        // binaryTree.deleteDeepestNode();
+        // binaryTree.levelOrderTraversal();
+        // System.out.println();
+
+        
+        binaryTree.deleteNode("N7");
+        binaryTree.levelOrderTraversal();
+        System.out.println();
+
+        binaryTree.deleteBT();
+        binaryTree.levelOrderTraversal();
+        System.out.println();
 
         
     }
